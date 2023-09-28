e30944c9f419a4e0639ae931b6eb3bfaa929cf02
Keipher-Mada <k.dubber17@gmail.com>
2023-09-28 11:14:47 +0100

    Create Weatherman.py

---
+++ Weatherman.py
@@ -0,0 +1,22 @@
+import requests
+
+api_key = '74d66f3d8fe5980c6bdd3deb97fb1783'
+
+#get input from user
+city = input('Enter city name: ')
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+#get info from url
+response = requests.get(url)
+
+#main code
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
+ 
